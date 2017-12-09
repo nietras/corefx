de95ed62498f356ff77a90ca95d879cb16fcb707
ntr <ntr@ihfood.dk>
2017-12-09 11:59:51 +0100

    revise ref api

--- src/System.Runtime.CompilerServices.Unsafe/ref/System.Runtime.CompilerServices.Unsafe.cs
+++ src/System.Runtime.CompilerServices.Unsafe/ref/System.Runtime.CompilerServices.Unsafe.cs
@@ -9,6 +9,24 @@ namespace System.Runtime.CompilerServices
 {
     public static partial class Unsafe
     {
+        // Add new
+        public static ref readonly T AddByteOffsetReadOnly<T>(in T source, System.IntPtr byteOffset) { throw null; }
+        public static ref readonly T AddReadOnly<T>(in T source, int elementOffset) { throw null; }
+        public static ref readonly T AddReadOnly<T>(in T source, System.IntPtr elementOffset) { throw null; }
+        public static ref readonly T AsRefReadOnly<T>(in T source) { throw null; }
+        public static ref readonly TTo AsReadOnly<TFrom, TTo>(in TFrom source) { throw null; }
+        public static ref readonly T SubtractByteOffsetReadOnly<T>(in T source, System.IntPtr byteOffset) { throw null; }
+        public static ref readonly T SubtractReadOnly<T>(in T source, int elementOffset) { throw null; }
+        public static ref readonly T SubtractReadOnly<T>(in T source, System.IntPtr elementOffset) { throw null; }
+
+        // Consider changing existing (problem this will not require `ref` when using do the `in` which makes it a bit weird
+        public static bool AreSame<T>(in T left, in T right) { throw null; }
+        public static System.IntPtr ByteOffset<T>(in T origin, in T target) { throw null; }
+        public static void CopyBlock(ref byte destination, in byte source, uint byteCount) { }
+        public static void CopyBlockUnaligned(ref byte destination, in byte source, uint byteCount) { }
+        public unsafe static void Copy<T>(void* destination, in T source) { }
+        public static T ReadUnaligned<T>(in byte source) { throw null; }
+
         public static ref T AddByteOffset<T>(ref T source, System.IntPtr byteOffset) { throw null; }
         public static ref T Add<T>(ref T source, int elementOffset) { throw null; }
         public unsafe static void* Add<T>(void* source, int elementOffset) { throw null; }
